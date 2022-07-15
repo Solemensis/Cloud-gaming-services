e3531bf3e3f8bdd27a6e846f7774b8b70f9fb670
Solemensis <96355888+Solemensis@users.noreply.github.com>
2022-07-15 03:22:25 +0300

    updates

--- script.js
+++ script.js
@@ -5,7 +5,3 @@ const heroSection = document.querySelector(".hero-section");
 window.onload = function () {
   heroSection.style.opacity = 1;
 };
-
-document.querySelector(".select").addEventListener("click", function () {
-  document.querySelector(".modal").showModal();
-});
