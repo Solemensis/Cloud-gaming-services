bce4207d4767235e2ce33de228d242727cc7d61e
Ulaş Saçlı <ulassacli@outlook.com>
2022-11-15 23:48:59 +0300

    ups

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 "use strict";
 
+// **** website first-load opacity animation ****
+
 const heroSection = document.querySelector(".hero-section");
 
 window.onload = function () {
   heroSection.style.opacity = 1;
 };
+
+// **** intersection observer to implement "scroll up" button ****
+
+const scrollUp = document.querySelector(".scroll-up");
+
+const stickyScrollUp = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) scrollUp.classList.add("opacity");
+  else scrollUp.classList.remove("opacity");
+};
+
+const buttonObserver = new IntersectionObserver(stickyScrollUp, {
+  root: null,
+  threshold: 0,
+});
+
+buttonObserver.observe(heroSection);
